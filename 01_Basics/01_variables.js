efccb0d89947373cbf1a2b3024782cb45c168867
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-10-13 21:35:28 +0530

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=14453
+let accounEmail="Hitesh@gmail.com"
+var accountPassword="1235"
+accountCity="Lucknow"
+
+
+// accountId = 2 This is not allowed in the javascript
+accounEmail="avc@gmai.com"
+accountPassword="23132123"
+accountCity="Bengaluru"
+let accState;
+console.log(accountId);
+accState="Hello World"
+accState=123456
+/* 
+Prefer not to use Var
+because of issue in block scope and function scope
+
+*/
+console.table([accounEmail,accountId,accountCity,accountPassword,accState]);
---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert("Hello world")
+
+ let name = "Suraj"
+ let age = 18
+ let isLoggIn = false
+ 
+
+// number=> 2 to power 53
+// bigint (used in the facebook and big tech)
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof undefined);
+
+//null is a object not type
