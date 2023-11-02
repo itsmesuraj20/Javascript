847a8c3e4ce3c8327f8f07941937280bf2a17f54
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-11-02 18:57:14 +0530

    Project 1 Done

---
+++ Freecodecamp/Javascript Programming/Project 2/script.js
@@ -0,0 +1,9 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let myname="Suraj"
+let greeting="Hello Everyone, "
+
+welcomeEl.innerText = greeting + myname 
+
+welcomeEl.innerText = welcomeEl.innerText + "✌"
+
